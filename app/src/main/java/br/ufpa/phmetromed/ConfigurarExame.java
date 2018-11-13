ff71e6c392e2f59ec58473053f426c19df644d90
Julia <juliahelena15@hotmail.com>
2018-11-13 19:14:07 -0300

    Initial commit

---
+++ app/src/main/java/br/ufpa/phmetromed/ConfigurarExame.java
@@ -0,0 +1,86 @@
+package br.ufpa.phmetromed;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ConfigurarExame extends AppCompatActivity {
+
+    private long id;
+    private String NomePaciente;
+    private Date DataNascPaciente;
+    private String Convenio;
+
+    private EditText editNomePaciente;
+    private EditText editDataNascPaciente;
+    private EditText editConvenio;
+
+    DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy"); // Make sure user insert date into edittext in this format.
+
+    private Button salvarCadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configurar_exame);
+
+        editNomePaciente = (EditText) findViewById(R.id.editNomePaciente);
+        editDataNascPaciente = (EditText) findViewById(R.id.editDataNascPaciente);
+        editConvenio = (EditText) findViewById(R.id.editConvenio);
+
+        this.salvarCadastro = (Button) findViewById(R.id.salvarCadastro);
+
+        this.salvarCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //executado ao clicar no botao
+            }
+        });
+
+    }
+
+
+    public void Salvar(View view) throws ParseException {
+
+        int test = 0;
+
+        if (this.editNomePaciente.getText().toString().isEmpty() == false) {
+            NomePaciente = this.editNomePaciente.getText().toString();
+        } else{
+            Toast.makeText(getApplicationContext(), "Preencha o Nome do Paciente...", Toast.LENGTH_LONG).show();
+            test = 1;
+        }
+        if(this.editDataNascPaciente.getText().toString().isEmpty() == false) {
+            String datanasc = this.editDataNascPaciente.getText().toString();
+            try {
+                DataNascPaciente = formatter.parse(datanasc);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        } else{
+            Toast.makeText(getApplicationContext(), "Preencha a Data de Nascimento...", Toast.LENGTH_LONG).show();
+            test = 1;
+        }
+        if (this.editConvenio.getText().toString().isEmpty() == false) {
+            Convenio = this.editConvenio.getText().toString();
+        } else{
+            Toast.makeText(getApplicationContext(), "Preencha o Convênio...", Toast.LENGTH_LONG).show();
+            test = 1;
+        }
+
+        if(test == 0){
+            // Emvia para o arduino
+            Toast.makeText(getApplicationContext(), "Enviar para o Arduino", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
---
+++ app/src/main/java/br/ufpa/phmetromed/MainActivity.java
@@ -0,0 +1,203 @@
+package br.ufpa.phmetromed;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int ENABLE_BLUETOOTH = 1;
+    public static int SELECT_PAIRED_DEVICE = 2;
+    public static int SELECT_DISCOVERED_DEVICE = 3;
+
+    static TextView statusMessage;
+    static TextView viewPH;
+
+    public String data_completa;
+    public Date data_atual;
+
+    ConnectionThread connect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        statusMessage = (TextView) findViewById(R.id.statusMessage);
+
+        viewPH = (TextView) findViewById(R.id.viewPH);
+
+
+        /* Teste rápido. O hardware Bluetooth do dispositivo Android
+            está funcionando ou está bugado?
+         */
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter == null) {
+            statusMessage.setText("Que pena! Hardware Bluetooth não está funcionando :(");
+        } else {
+            statusMessage.setText("Ótimo! Hardware Bluetooth está funcionando :D");
+        }
+
+        if(!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH);
+            statusMessage.setText("Solicitando ativação do Bluetooth...");
+        } else {
+            statusMessage.setText("Bluetooth já ativado :)");
+        }
+
+        /* Um descanso rápido, para evitar bugs.
+         */
+        try {
+            Thread.sleep(1000);
+        } catch (Exception E) {
+            E.printStackTrace();
+        }
+    }
+
+    public void tempo(){
+        try{
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            // OU
+            //SimpleDateFormat dateFormat_hora = new SimpleDateFormat("HH:mm:ss");
+
+            Date data = new Date();
+
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(data);
+            data_atual = cal.getTime();
+
+            data_completa = dateFormat.format(data_atual);
+
+            //hora_atual = dateFormat_hora.format(data_atual);
+
+            Log.i("data_completa", data_completa);
+            //Log.i("data_atual", data_atual.toString());
+            //Log.i("hora_atual", hora_atual); // Esse é o que você quer
+        }catch (Exception e){
+
+        }
+
+    }
+
+    public void salvar(String texto){
+        tempo();
+        try{
+            if (true){ //função de salvar
+                Toast.makeText(MainActivity.this,"Salvo com sucesso!",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(MainActivity.this,"Erro ao salvar!",Toast.LENGTH_SHORT).show();
+            }
+        }catch (Exception e) {
+            Toast.makeText(MainActivity.this, "Erro ao salvar!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public static Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            /* Esse método é invocado na Activity principal
+                sempre que a thread de conexão Bluetooth recebe
+                uma mensagem.
+             */
+            Bundle bundle = msg.getData();
+            byte[] data = bundle.getByteArray("data");
+            String dataString= new String(data);
+
+            /* Aqui ocorre a decisão de ação, baseada na string
+                recebida. Caso a string corresponda à uma das
+                mensagens de status de conexão (iniciadas com --),
+                atualizamos o status da conexão conforme o código.
+             */
+            if(dataString.equals("---N"))
+                statusMessage.setText("Ocorreu um erro durante a conexão D:");
+            else if(dataString.equals("---S"))
+                statusMessage.setText("Conectado :D");
+            else {
+                if(dataString.contains("PH")){
+                    viewPH.setText(dataString);
+                }else{
+
+                }
+                /* Se a mensagem não for um código de status,
+                    então ela deve ser tratada pelo aplicativo
+                    como uma mensagem vinda diretamente do outro
+                    lado da conexão. Nesse caso, simplesmente
+                    atualizamos o valor contido no TextView do
+                    contador.
+                 */
+            }
+
+        }
+    };
+
+    public void searchPairedDevices(View view) {
+
+        Intent searchPairedDevicesIntent = new Intent(this, PairedDevices.class);
+        startActivityForResult(searchPairedDevicesIntent, SELECT_PAIRED_DEVICE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == ENABLE_BLUETOOTH) {
+            if(resultCode == RESULT_OK) {
+                statusMessage.setText("Bluetooth ativado :D");
+            }
+            else {
+                statusMessage.setText("Bluetooth não ativado :(");
+            }
+        }
+        else if(requestCode == SELECT_PAIRED_DEVICE || requestCode == SELECT_DISCOVERED_DEVICE) {
+            if (resultCode == RESULT_OK) {
+                statusMessage.setText("Você selecionou " + data.getStringExtra("btDevName") + "\n"
+                        + data.getStringExtra("btDevAddress"));
+
+                connect = new ConnectionThread(data.getStringExtra("btDevAddress"));
+                connect.start();
+            } else {
+                statusMessage.setText("Nenhum dispositivo selecionado :(");
+            }
+        }
+    }
+}
+
